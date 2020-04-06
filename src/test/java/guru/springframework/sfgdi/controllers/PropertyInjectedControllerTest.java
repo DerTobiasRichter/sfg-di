425ffe6dc4d61bea674c8149154acaf755d910a5
Tobias Richter <der.tobias.richter@googlemail.com>
2020-04-06 15:46:52 +0200

    34. Dependency Injection without Spring: End State

---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController propertyInjectedController;
+
+
+    @BeforeEach
+    void setup(){
+        propertyInjectedController = new PropertyInjectedController();
+
+        propertyInjectedController.greetingService = new GreetingServiceImpl();
+
+    }
+
+    @Test
+    void getGreeting(){
+        System.out.println(propertyInjectedController.getGreeting());
+    }
+
+}
